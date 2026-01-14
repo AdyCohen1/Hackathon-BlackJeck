cf3e1152da9a135702bce3dbfe205016e19c9fd0
shay7210 <shay7210@gmail.com>
2026-01-14 21:08:17 +0200

    commited changes

--- Client/web_game/static/app.js
+++ Client/web_game/static/app.js
@@ -182,7 +182,7 @@ setInterval(async () => {
 
         if (data.win_rate >= 0){
             console.log(data.win_rate)
-            document.getElementById("win-rate").innerText = `${data.win_rate}`
+            document.getElementById("win-rate").innerText = `${data.win_rate}%`
         }
 
     } catch (e) {
